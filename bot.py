1e35354e1d9096cb138a77516f606464a81554de
xEfren548 <efren.alvarez@iteso.mx>
2025-02-27 23:28:32 -0600

    first commit

---
+++ bot.py
@@ -0,0 +1,92 @@
+from telethon import TelegramClient, events
+import asyncio
+import aiohttp  # Para hacer solicitudes HTTP asíncronas
+from datetime import datetime, timedelta
+import pytz  # Para manejar zonas horarias
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+
+
+# Configuración
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+phone_number  = os.getenv('PHONE_NUMBER')
+channel = os.getenv('CHANNEL')
+webhook_url = os.getenv('WEBHOOK_URL') # URL del servidor Node.js
+
+client = TelegramClient("session", api_id, api_hash)
+
+async def send_to_webhook(message, message_date):
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(webhook_url, json={"message": message, "date": message_date}) as response:
+                if response.status == 200:
+                    print("Mensaje enviado al servidor Node.js")
+                else:
+                    print(f"Error al enviar el mensaje: {response.status}")
+        except Exception as e:
+            print(f"Error en la solicitud HTTP: {e}")
+
+''' Mensajes en tiempo real '''
+
+async def main():
+    await client.start(phone_number)  # Iniciar sesión
+    me = await client.get_me()  # Obtener información de la cuenta
+    print(f"Conectado como {me.username}")
+
+    # Escuchar mensajes en tiempo real
+    @client.on(events.NewMessage(chats=channel))
+    async def handler(event):
+        message_text = event.message.text
+        message_date = event.message.date
+
+        utc_date = message_date.replace(tzinfo=pytz.UTC)  # Asegurar que la fecha esté en UTC
+        local_date = utc_date.astimezone(pytz.timezone("America/Mexico_City"))
+        formatted_date = local_date.strftime("%Y-%m-%d %H:%M:%S")
+
+
+        print(f"Nuevo mensaje en {channel}: {formatted_date} el {message_date}")
+        await send_to_webhook(message_text, formatted_date)  # Enviar mensaje al servidor Node.js
+
+    await client.run_until_disconnected()
+
+asyncio.run(main())
+
+
+''' Obtener mensajes anteriores '''
+
+# async def get_previous_messages():
+#     try:
+#         await client.start(PHONE_NUMBER)  # Iniciar sesión
+#         me = await client.get_me()  # Obtener información de la cuenta
+#         print(f"Conectado como {me.username}")
+
+#         # Obtener la entidad del canal
+#         try:
+#             entity = await client.get_entity(CHANNEL)
+#             print(f"Canal encontrado: {entity.title}")
+#         except Exception as e:
+#             print(f"No se pudo encontrar el canal: {e}")
+#             return
+
+#         # Calcular la fecha de hace X días
+#         days_ago = 2  # Número de días atrás
+#         from_date = datetime.now() - timedelta(days=days_ago)
+
+#         # Obtener mensajes anteriores
+#         messages = await client.get_messages(entity, offset_date=from_date, limit=10)  # Limitar a 10 mensajes
+#         for message in messages:
+#             print(f"Mensaje de {message.date}: {message.text}")
+#             print(f"Fecha: {event.message}")
+
+#             await send_to_webhook(message.text)  # Enviar mensaje al servidor Node.js
+
+#     except Exception as e:
+#         print(f"Error en el bot: {e}")
+#     finally:
+#         await client.disconnect()
+
+# asyncio.run(get_previous_messages())
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const moment = require("moment");
+const momentTz = require("moment-timezone");
+
+const app = express();
+const PORT = 4005;
+
+// Middleware para parsear el cuerpo de las solicitudes JSON
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.get("/contenido", (req, res) => {
+    res.sendFile(__dirname + "/public/contenidoDiario.html");
+});
+
+let messages = [];
+
+app.post("/webhook", (req, res) => {
+    const { message, date } = req.body;
+
+    messages.push({ text: message, date: date }); // Almacenar el mensaje
+    console.log(`Mensaje recibido (${date}): ${message}`);
+
+    res.status(200).send("Mensaje recibido");
+});
+
+app.get("/messages", (req, res) => {
+    // const today = new Date().toISOString().split("T")[0]; // Obtener la fecha actual (YYYY-MM-DD)
+    const today = momentTz().tz("America/Mexico_City").format("YYYY-MM-DD");
+    // const todayMessages = messages.filter(msg => msg.date === today); // Filtrar mensajes del día actual
+
+    const todayMessages = messages.filter(msg => {
+        const messageDate = msg.date.split(" ")[0]; // Extraer la parte de la fecha (YYYY-MM-DD)
+        return messageDate === today;
+    });
+
+    res.json(todayMessages); // Enviar los mensajes almacenados
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor Node.js escuchando en http://localhost:${PORT}`);
+});
